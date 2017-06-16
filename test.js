3a39025ae1523c2f39a39c33480b27c8a73c717f
Dave Winer <dave.winer@gmail.com>
2017-06-16 15:26:29 -0400

    V0.4.0

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 const opmlToJs = require ("./opmltojs.js");
 const request = require ("request");
+const fs = require ("fs");
 request ("http://this.how/electricRiver?format=opml", function (error, response, data) {
 	opmlToJs.parse (data, function (theOutline) {
-		console.log (JSON.stringify (theOutline, undefined, 4));
+		fs.writeFile ("test.json", JSON.stringify (theOutline, undefined, 4))
 		});
 	});
